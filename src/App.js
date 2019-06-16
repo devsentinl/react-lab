a28faeed8041ac7f11948a6ce164bb48729f91cd
unknown <oltursa.developer@gmail.com>
2019-06-16 11:50:06 -0500

    temp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MenuComponent from './components/menu/MenuComponent'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <MenuComponent/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/menu/MenuComponent.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+
+class MenuComponent extends Component{
+    render()
+    {
+        return <div>
+            MENU 
+        </div>
+    }
+}
+export default MenuComponent
